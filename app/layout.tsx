b2003f92fa950fb84a6df01dd9ed1c0091f82c74
Mehdi Tareghi <mehditareghi@gmail.com>
2024-11-04 11:07:49 +0330

    add search console

--- app/layout.tsx
+++ app/layout.tsx
@@ -91,6 +91,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         href={`${basePath}/static/favicons/safari-pinned-tab.svg`}
         color="#5bbad5"
       />
+      <meta name="google-site-verification" content="YpSvBZAqhNlGv2sJaMgcQyJyaSSbBrfyQZ8a26IKzJg" />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
